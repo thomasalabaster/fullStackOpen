5aa52ec8b7c115f383d33f2edc141344f0da13b2
thomasalabaster <tomalabaster@gmail.com>
2023-05-24 13:00:22 -0700

    4.2 Blog list, step2

---
+++ part4/blogs/controllers/blogs.js
@@ -0,0 +1,36 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(() => {
+      console.log("not succesful")
+    })
+})
+
+blogsRouter.get('/:id', (request, response) => {
+  Blog
+    .findById(request.params.id)
+    .then(blog => {
+      response.json(blog)
+    })
+    .catch(() => {
+      console.log("not succesful")
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+
+module.exports = blogsRouter
